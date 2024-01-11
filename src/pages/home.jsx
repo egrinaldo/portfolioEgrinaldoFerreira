3bf0fd57f9c9fab935002979e4230be85b7e2295
Egrinaldo Junior <egrinaldo19@gmail.com>
2024-01-10 21:57:24 -0300

    revisao

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import { BodyIni } from "../Components/BodyIni/BodyIni"
+import { Intro } from "../Components/Intro/intro"
+import { Menu } from "../Components/Menu/Menu"
+
+export const Home = () => {
+
+    return (
+
+        <><Menu /><BodyIni /><Intro /></>
+
+    
+    )
+
+}
